f7c2dead9356bc5ddc6f3e6963318c4f61177920
zoeysann <safarovazahra1@gmail.com>
2023-11-27 06:54:31 +0400

    {done}

--- Dict-HW/dict1-4.py
+++ Dict-HW/dict1-4.py
@@ -5,12 +5,15 @@ dict = {
     "personality": "artsy",
     "city": "Baku",
     }
+
 # 1st-print, 2nd-add, 3rd-remove 
 print(dict["name"])
+
 # simple way, using update() or setdefault() methods
 dict["occopuation"]= "study"
 dict.update({"occopuation":"study"})
 dict.setdefault("occopuation", "study")
+
 # pop() method or del built-in function
 dict.pop("city")
 del dict["city"]
